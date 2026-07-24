13f224354664cc4869a3086112776789dbde18b5
Juan Santos <juan.moreira62@outlook.com>
2026-07-24 20:33:02 -0300

    fix: inclui pasta dist para exibicao correta no gh pages

---
+++ dist/script.d.ts
@@ -0,0 +1,10 @@
+declare global {
+    interface Window {
+        vlibras?: {
+            translate: (texto: string) => void;
+            play: () => void;
+        };
+    }
+}
+export {};
+//# sourceMappingURL=script.d.ts.map
---
+++ dist/types.d.ts
@@ -0,0 +1,6 @@
+export interface SinalLibras {
+    letra: string;
+    caminhoImg: string;
+    descricaoAl: string;
+}
+//# sourceMappingURL=types.d.ts.map
